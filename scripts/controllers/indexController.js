11906de101d00813baa41822a3ad5c4bb58d6d51
Callum Brown <callumaidanbrown@gmail.com>
2017-11-05 21:41:34 +0000

    Opening modal no longer redirects (uses local function instead of materialize routes)

--- scripts/controllers/indexController.js
+++ scripts/controllers/indexController.js
@@ -1,54 +1,57 @@
-binaryTestApp.controller('indexController', ["$scope", "$location", "_dataContext", function ($scope, $location, context) {
+binaryTestApp.controller('indexController', ["$scope", "$location", "_dataContext", function($scope, $location, context) {
 
-	$scope.activeView = sessionStorage.getItem("storedPath");
+    $scope.activeView = sessionStorage.getItem("storedPath");
 
-	$(function () { // Initialise modal
-		$('#optionsModal').modal();
-	});
+    $(function() { // Initialise modal
+        $('#optionsModal').modal();
+    });
 
-	$scope.dismiss = function () {
-		$('#optionsModal').modal('close');
-	};
+    $scope.dismiss = function() {
+        $('#optionsModal').modal('close');
+    };
 
-	$scope.changeView = function (path, data) {
+    $scope.changeView = function(path, data) {
 
-		if (!data) {
-			$scope.activeView = path;
-		}
+        if (!data) {
+            $scope.activeView = path;
+        }
 
-		sessionStorage.setItem("storedPath", path);
+        sessionStorage.setItem("storedPath", path);
 
-		$location.path(path + "/" + (data ? data.Id : ""));
-		$(".popover").hide();
-	};
+        $location.path(path + "/" + (data ? data.Id : ""));
+        $(".popover").hide();
+    };
 
-	$scope.updateDifficultyModal = function (data) {
-		if (data != null) {
-			sessionStorage.setItem("Difficulty", data);
-			if (data == "easy") {
-				$("#submit-btn-modal").prop("disabled", false);
-				document.getElementById("submit-btn-test-modal").style.display = "none";
-				document.getElementById("submit-btn-test-modal-row").style.display = "none";
-				document.getElementById("submit-btn-modal").style.display = "block";
-				document.getElementById("submit-btn-modal-row").style.display = "block";
-				document.getElementById("easyDifficultyModal").style.boxShadow = "0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
-				document.getElementById("mediumDifficultyModal").style.boxShadow = "none";
-			} else {
-				$("#submit-btn-test-modal").prop("disabled", false);
-				document.getElementById("submit-btn-modal").style.display = "none";
-				document.getElementById("submit-btn-modal-row").style.display = "none";
-				document.getElementById("submit-btn-test-modal").style.display = "block";
-				document.getElementById("submit-btn-test-modal-row").style.display = "block";
-				document.getElementById("mediumDifficultyModal").style.boxShadow = "0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
-				document.getElementById("easyDifficultyModal").style.boxShadow = "none";
-			}
-		}
-	};
+    $scope.openModal = function() {
+        $('#optionsModal').modal('open');
+    };
+    $scope.updateDifficultyModal = function(data) {
+        if (data != null) {
+            sessionStorage.setItem("Difficulty", data);
+            if (data == "easy") {
+                $("#submit-btn-modal").prop("disabled", false);
+                document.getElementById("submit-btn-test-modal").style.display = "none";
+                document.getElementById("submit-btn-test-modal-row").style.display = "none";
+                document.getElementById("submit-btn-modal").style.display = "block";
+                document.getElementById("submit-btn-modal-row").style.display = "block";
+                document.getElementById("easyDifficultyModal").style.boxShadow = "0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+                document.getElementById("mediumDifficultyModal").style.boxShadow = "none";
+            } else {
+                $("#submit-btn-test-modal").prop("disabled", false);
+                document.getElementById("submit-btn-modal").style.display = "none";
+                document.getElementById("submit-btn-modal-row").style.display = "none";
+                document.getElementById("submit-btn-test-modal").style.display = "block";
+                document.getElementById("submit-btn-test-modal-row").style.display = "block";
+                document.getElementById("mediumDifficultyModal").style.boxShadow = "0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
+                document.getElementById("easyDifficultyModal").style.boxShadow = "none";
+            }
+        }
+    };
 
-	$(document).on('click', '#toast-container .toast', function () {
-		$(this).fadeOut(function () {
-			$(this).remove();
-		});
-	});
+    $(document).on('click', '#toast-container .toast', function() {
+        $(this).fadeOut(function() {
+            $(this).remove();
+        });
+    });
 
 }]);
